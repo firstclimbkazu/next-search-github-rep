cd13d40ba9c9aa785c54a2b8f79ea3ddc636255c
firstclimbkazu <firstclimbkazu@gmail.com>
2025-07-21 15:01:17 +0900

    各種設定ファイルを更新

--- next-search-github-rep/next.config.ts
+++ next-search-github-rep/next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  images: {
+    // リモート画像のホスト名を許可する設定
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**', // GitHubのアバター画像のパスは /u/ から始まることが多い
+      },
+    ],
+  },
 };
 
 export default nextConfig;
